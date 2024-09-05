3675c32f331d7664e8e47d62a31704488a93bd20
Oleksandr Tukhovskyi <alexx1984@ukr.net>
2024-09-05 13:39:13 +0300

    entitis created

---
+++ app/Http/Controllers/SharedAccessController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SharedAccess;
+use Illuminate\Http\Request;
+
+class SharedAccessController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SharedAccess $sharedAccess)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SharedAccess $sharedAccess)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SharedAccess $sharedAccess)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SharedAccess $sharedAccess)
+    {
+        //
+    }
+}
