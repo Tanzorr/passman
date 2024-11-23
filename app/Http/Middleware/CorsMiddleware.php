2e25f628565463c783838bccb9bd997a7ece44f5
Oleksandr Tukhovskyi <alexx1984@ukr.net>
2024-11-23 17:48:26 +0200

    add  passwords seeders

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -21,10 +21,10 @@ class CorsMiddleware
             return response()->json([], 200);
         }
 
-        // cors config should be taken from config files, so you can have control over in in entire app.
-        //        $response->headers->set('Access-Control-Allow-Origin', '*');
-        //        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        //        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+       //  cors config should be taken from config files, so you can have control over in in entire app.
+//                $response->headers->set('Access-Control-Allow-Origin', '*');
+//                $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//                $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $next($request);
     }
